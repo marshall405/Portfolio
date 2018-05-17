c173cd5062bc05e5811ba770365d57e8eaf2dab3
Marshall Slemp <marshall.slemp@gmail.com>
2018-05-17 17:42:20 -0500

    change main1.js to main.js

--- main.js
+++ main.js
@@ -3,76 +3,40 @@ window.onload = function() {
 	const increase = .5;
 	const time = 10;
 	const skills = {
-		html: 85,
-		css: 75,
-		javascript: 52,
-		commandline: 65
+		'html': 85,
+		'css': 75,
+		'javascript': 52,
+		'commandline': 65
 	};
 	function getID(element) {
 		return document.getElementById(element);
 	}
-	//javascript bar
-	(function() {
-		let innerJSBar = getID("innerJSBar");
-		innerJSBar.style.width = "0";
+	function skillBar(skill, element){
+		let bar = getID(element);
 		let increment = 0;
 		let timer = setInterval(function() {
-			if(increment >= skills.javascript) {
+			if(increment >= skills[skill]) {
 				clearInterval(timer);
 			};
-			innerJSBar.style.width = increment + "%";
+			bar.style.width = increment + "%";
 			increment += increase;
 		}, time);
-	})();
-	//html bar
-	(function() {
-		let innerHTMLBar = getID("innerHTMLBar");
-		innerHTMLBar.style.width = "0";
-		let increment = 0;
-		let timer = setInterval(function() {
-			if(increment >= skills.html) {
-				clearInterval(timer);
-			};
-			innerHTMLBar.style.width = increment + "%";
-			increment += increase;
-		}, time);
-	})();
-	//css bar
-	(function() {
-		let innerCSSBar = getID("innerCSSBar");
-		innerCSSBar.style.width = "0";
-		let increment = 0;
-		let timer = setInterval(function(){
-			if(increment >= skills.css) {
-				clearInterval(timer);
-			};
-			innerCSSBar.style.width = increment + "%";
-			increment += increase;
-		}, time);
-	})();
-	//command line bar
-	(function() {
-		let innerCLBar = getID("innerCLBar");
-		innerCLBar.style.width = "0";
-		let increment = 0;
-		let timer = setInterval(function(){
-			if(increment >= skills.commandline) {
-				clearInterval(timer);
-			};
-			innerCLBar.style.width = increment + "%";
-			increment += increase;
-		}, time);
-	})();
-
-	// Set CSS for skill bars
-	// #htmlSkill, #cssSkill, #clSkill, #jsSkill
-	{
-		let htmlSkill = document.getElementById('htmlSkill');
-		let cssSkill = document.getElementById('cssSkill');
-		let clSkill = document.getElementById('clSkill');
-		let jsSkill = document.getElementById('jsSkill');
-
-
 	}
+	skillBar('html', 'innerHTMLBar');
+	skillBar('javascript', 'innerJSBar');
+	skillBar('css', 'innerCSSBar');
+	skillBar('commandline', 'innerCLBar');
 
+
+	// Set CSS left position for skill bar percentages
+	// #htmlSkill, #cssSkill, #clSkill, #jsSkill
+	function setCss(skill, element) {
+		let ele = getID(element);
+		ele.innerHTML = skills[skill] + '%';
+		ele.style.left = skills[skill] + '%';
+	}
+	setCss('html', 'htmlSkill');
+	setCss('css', 'cssSkill');
+	setCss('commandline', 'clSkill');
+	setCss('javascript' ,'jsSkill');
 };
