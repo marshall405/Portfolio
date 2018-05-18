7373e47fc3c7f0e15d6877a2bcebf83328a2d314
Marshall Slemp <marshall.slemp@gmail.com>
2018-05-18 01:47:17 -0500

    new profile picture/box shadows every where

--- main.js
+++ main.js
@@ -1,42 +1,95 @@
+// version 2
 window.onload = function() {
 	//settings
 	const increase = .5;
 	const time = 10;
 	const skills = {
-		'html': 85,
-		'css': 75,
-		'javascript': 52,
-		'commandline': 65
+		html: {
+			rating: 85,
+			name: 'HTML',
+			level: 'Advanced'
+		},
+		css: {
+			rating: 75,
+			name: 'CSS',
+			level: 'Advanced'
+		},
+		javascript: {
+			rating: 52,
+			name: 'JavaScript',
+			level: 'Intermediate'
+		},
+		commandline: {
+			rating: 65,
+			name: 'Command Line',
+			level: 'Intermediate'
+		},
+		nodejs: {
+			rating: 50,
+			name: 'Node JS',
+			level: 'Intermediate'
+		},
+		express: {
+			rating: 45,
+			name: 'Express',
+			level: 'Intermediate'
+		}
 	};
-	function getID(element) {
-		return document.getElementById(element);
+	const mainDiv = document.getElementById('skills');
+
+	(function(){
+		for (var skill in skills){
+			buildSkillBar(skills[skill]);
+		}
+	})();
+
+	function buildSkillBar(skill) {
+		let outerDiv 	= 	createElement('div');
+		let h4 			= 	createElement('h4');
+		let innerDiv 	= 	createElement('div');
+		let skillBarDiv = 	createElement('div');
+		let skillRating = 	createElement('div');
+		let skillLevel 	=	createElement('p');
+
+		mainDiv.appendChild(outerDiv);
+
+		outerDiv.appendChild(h4);
+		outerDiv.appendChild(innerDiv);
+		outerDiv.appendChild(skillLevel);
+
+		innerDiv.appendChild(skillBarDiv);
+		innerDiv.appendChild(skillRating);
+
+		h4.innerHTML = skill.name;
+		skillLevel.innerHTML = skill.level;
+		skillRating.innerHTML = skill.rating + '%';
+		skillRating.setAttribute('class', 'hideSkillRating');
+
+		innerDiv.setAttribute('class', 'skillBar');
+		skillBarDiv.setAttribute('class', 'barSkill');
+
+		growSkillBar(skill, skillBarDiv);
+		skillRating.style.left = skill.rating + '%';
+		skillBarDiv.addEventListener('mouseover', function(){
+			skillRating.setAttribute('class', 'showSkillRating');
+		});
+		skillBarDiv.addEventListener('mouseleave', function() {
+			skillRating.setAttribute('class', 'hideSkillRating');
+		});
 	}
-	function skillBar(skill, element){
-		let bar = getID(element);
+
+
+	function createElement(element){
+		return document.createElement(element);
+	}
+	function growSkillBar(skill, element){
 		let increment = 0;
 		let timer = setInterval(function() {
-			if(increment >= skills[skill]) {
+			if(increment >= skill.rating) {
 				clearInterval(timer);
 			};
-			bar.style.width = increment + "%";
+			element.style.width = increment + "%";
 			increment += increase;
 		}, time);
 	}
-	skillBar('html', 'innerHTMLBar');
-	skillBar('javascript', 'innerJSBar');
-	skillBar('css', 'innerCSSBar');
-	skillBar('commandline', 'innerCLBar');
-
-
-	// Set CSS left position for skill bar percentages
-	// #htmlSkill, #cssSkill, #clSkill, #jsSkill
-	function setCss(skill, element) {
-		let ele = getID(element);
-		ele.innerHTML = skills[skill] + '%';
-		ele.style.left = skills[skill] + '%';
-	}
-	setCss('html', 'htmlSkill');
-	setCss('css', 'cssSkill');
-	setCss('commandline', 'clSkill');
-	setCss('javascript' ,'jsSkill');
 };
