ced77d34b3f4e7065c6b283acd484db03c135fef
Marshall Slemp <marshall.slemp@gmail.com>
2018-05-24 23:40:32 -0500

    put skills object in seperate file

--- main.js
+++ main.js
@@ -4,40 +4,8 @@ window.onload = function() {
 
 	const increase = .5;
 	const time = 10;
-	const skills = {
-		html: {
-			rating: 85,
-			name: 'HTML',
-			level: 'Advanced'
-		},
-		css: {
-			rating: 60,
-			name: 'CSS',
-			level: 'Advanced'
-		},
-		javascript: {
-			rating: 62,
-			name: 'JavaScript',
-			level: 'Intermediate'
-		},
-		commandline: {
-			rating: 65,
-			name: 'Command Line',
-			level: 'Intermediate'
-		},
-		nodejs: {
-			rating: 50,
-			name: 'Node JS',
-			level: 'Intermediate'
-		},
-		express: {
-			rating: 34,
-			name: 'Express',
-			level: 'Intermediate'
-		}
-	};
 	const mainDiv = document.getElementById('skills');
-
+	console.log(skills);
 	(function(){
 		for (var skill in skills){
 			buildSkillBar(skills[skill]);
---
+++ skills.js
@@ -0,0 +1,34 @@
+// Add new skill to skills object
+
+let skills = {
+	html: {
+		rating: 85,
+		name: 'HTML',
+		level: 'Advanced'
+	},
+	css: {
+		rating: 60,
+		name: 'CSS',
+		level: 'Advanced'
+	},
+	javascript: {
+		rating: 62,
+		name: 'JavaScript',
+		level: 'Intermediate'
+	},
+	commandline: {
+		rating: 65,
+		name: 'Command Line',
+		level: 'Intermediate'
+	},
+	nodejs: {
+		rating: 50,
+		name: 'Node JS',
+		level: 'Intermediate'
+	},
+	express: {
+		rating: 34,
+		name: 'Express',
+		level: 'Intermediate'
+	}	
+};
