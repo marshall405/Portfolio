d7980caefac6ef5cc61b790e026ca740e3806b8d
Marshall Slemp <marshall.slemp@gmail.com>
2018-05-28 17:49:12 -0500

    initial commit

---
+++ education.js
@@ -0,0 +1,43 @@
+const education = [
+	{
+		name: 'Codecademy',
+		courses: [
+			{
+				courseName: 'Learn Sass',
+				completion: 100
+			},
+			{
+				courseName: 'Learn HTML',
+				completion: 100
+			},
+			{
+				courseName: 'Learn JavaScript',
+				completion: 100
+			},
+			{
+				courseName: 'Learn Python',
+				completion: 100
+			},
+			{
+				courseName: 'Learn the Command Line',
+				completion: 100
+			},
+			{
+				courseName: 'Make a Website',
+				completion: 100
+			}
+		],
+		degree: false,
+		major: false,
+		graduation: false
+	},
+	{
+		name: 'Oklahoma State University',
+		courses: false,
+		degree: 'Bachelor of Science, Business Administration',
+		major: 'General Business',
+		graduation: 'December 2010'
+	}
+]
+
+
